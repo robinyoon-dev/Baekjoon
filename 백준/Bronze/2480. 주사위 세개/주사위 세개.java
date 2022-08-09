58ca9c241093bc7b9e70f37268980ea04ce921de
Robin Yoon <107087958+robinyoon-dev@users.noreply.github.com>
2022-08-10 08:53:04 +0900

    [Bronze IV] Title: 주사위 세개, Time: 204 ms, Memory: 17740 KB -BaekjoonHub

---
+++ 백준/Bronze/2480. 주사위 세개/주사위 세개.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		int A = scanner.nextInt();
+		int B = scanner.nextInt();
+		int C = scanner.nextInt();
+		
+		if( A == B && A == C && B == C) {
+			
+			System.out.println(10000+A*1000);
+			
+		}else if(A == B || A == C) {
+			System.out.println(1000+A*100);
+		}else if(B == C) {
+			System.out.println(1000+B*100);
+		}else {
+			if(A > B && A > C) {
+				System.out.println(A*100);
+			}else if(B > A && B > C) {
+				System.out.println(B*100);
+			}else {
+				System.out.println(C*100);
+			}
+		}
+		
+	}
+
+}
