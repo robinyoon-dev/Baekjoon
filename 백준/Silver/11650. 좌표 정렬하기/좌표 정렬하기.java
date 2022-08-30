cbd37368cdee4ee61402fcb4e24b139243eb5363
Robin Yoon <107087958+robinyoon-dev@users.noreply.github.com>
2022-08-30 15:38:47 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 2224 ms, Memory: 192296 KB -BaekjoonHub

---
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.java
@@ -0,0 +1,38 @@
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Scanner;
+
+public class Main{
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in); 
+		
+		int N = sc.nextInt(); 
+				
+		int [][] array = new int [N][2];
+		
+		for(int i = 0; i < N; i++) {
+			array[i][0] = sc.nextInt();
+			array[i][1] = sc.nextInt();
+		}
+		
+		
+		Arrays.sort(array, new Comparator<int[]>() {
+
+			@Override
+			public int compare(int[] o1, int[] o2) {
+				
+				return o1[0]!=o2[0] ? o1[0]-o2[0] : o1[1] - o2[1];
+			}					
+			
+		});
+		
+		for(int i = 0; i < N ; i++) {
+			System.out.println(array[i][0]+" "+array[i][1]);		
+		}
+		
+
+		
+					
+	}
+}
