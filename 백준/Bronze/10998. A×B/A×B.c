dfc30cedcb0b516229c3223ef1ecf344aea8558a
Robin Yoon <107087958+robinyoon-dev@users.noreply.github.com>
2022-09-23 17:05:45 +0900

    [Bronze V] Title: A×B, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/10998. A×B/A×B.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main(void)
+{
+    int A;
+    int B;
+    int mul;
+    
+    scanf("%d", &A);
+    scanf("%d", &B);
+    
+    mul = A*B;
+    
+    printf("%d", mul);
+    
+    return 0;
+}
