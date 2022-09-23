50716ece54a3e1a4675fe67f4eb987b30a10e9f8
Robin Yoon <107087958+robinyoon-dev@users.noreply.github.com>
2022-09-23 17:00:58 +0900

    [Bronze V] Title: A+B, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main(void)
+{
+
+    int A;
+    int B;
+    int total;
+    
+    scanf("%d", &A);
+    scanf("%d", &B);
+    
+    total = A + B;
+    printf("%d", total);
+    
+    return 0;
+    
+}
